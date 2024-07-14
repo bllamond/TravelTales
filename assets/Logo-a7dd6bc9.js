55c5ec5f1fba9cefb7ca8eaec44ea2cb46cd8fdd
bllamond <vinaymalik1729@gmail.com>
2024-07-14 11:51:34 +0530

    Updates

---
+++ assets/Logo-a7dd6bc9.js
@@ -0,0 +1,1 @@
+import{j as o,L as s}from"./index-1041df47.js";const l="_logo_7b2ls_1",t={logo:l};function n(){return o.jsx(s,{to:"/",children:o.jsx("img",{src:"/logo1.png",alt:"WorldWise logo",className:t.logo})})}export{n as L};
